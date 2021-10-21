9129eaab755a251270829f2251deee9b35d428ea
RDrivemirror2 <90256011+RDrivemirror2@users.noreply.github.com>
2021-10-21 13:26:12 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Channel", "https://t.me/Namexian")
-    buttons.buildbutton("Group", "https://t.me/EunhaMirror")
+    buttons.buildbutton("Channel", "https://t.me/RdriveMirror")
+    buttons.buildbutton("Group", "https://t.me/joinchat/373jsffwYURiMDBl")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
